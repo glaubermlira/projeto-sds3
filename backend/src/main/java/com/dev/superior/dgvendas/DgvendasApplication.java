108d0bade6ddb116ea66a7526b5d294d36b0f52c
glaubermlira <glauberdevmlira@gmail.com>
2021-05-03 22:24:52 -0300

    Project Created

---
+++ backend/src/main/java/com/dev/superior/dgvendas/DgvendasApplication.java
@@ -0,0 +1,13 @@
+package com.dev.superior.dgvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/superior/dgvendas/DgvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.superior.dgvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
