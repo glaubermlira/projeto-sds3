cf1dd17b0e1b433547f74421270f60a2fe93cc6e
glaubermlira <glauberdevmlira@gmail.com>
2021-05-05 23:58:52 -0300

    Basic static components

---
+++ frontend/src/assets/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/glaubermlira/" target="_blank" rel="noreferrer">Glauber de Lira</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
+
